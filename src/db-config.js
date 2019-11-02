ae1eeef2349958ce054b9a654a6429cca5c413e5
Norlock <jwillems25@hotmail.com>
2019-11-02 04:16:02 +0100

    eindelijk kut db created

---
+++ src/db-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'url': 'http://127.0.0.1:8529', // The default URL for a local server
+
+    // Database user credentials to use
+    'username': 'root',
+    'password': 'openSesame'
+};
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const Database = require('arangojs').Database;
 
-db = new Database();
-db.createDatabase('owdb').then(
+const db = new Database();
+db.useBasicAuth('root', 'openSesame')
+console.log('db', db);
+
+db.createDatabase('orb_weaver_db').then(
   () => console.log('Database created'),
   err => console.error('Failed to create database:', err)
 );
